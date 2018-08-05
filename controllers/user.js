47a9cf027da4263839986a069fb195e5d790272a
Mehmet Cetin <mcetin.cm@gmail.com>
2018-08-05 17:54:49 +0400

    hashes password on user register

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,21 @@
 const { pick } = require('lodash');
+const bcrypt = require('bcrypt');
 const { User } = require('../models/user');
 const { logger } = require('../logger');
 
 const register = (req, res) => {
   const { name, email, password } = req.body || {};
 
-  User.create({ name, email, password })
+  let hashedPassword;
+  if (password) {
+    hashedPassword = bcrypt.hashSync(password, 8);
+  }
+
+  User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
     .then((user) => {
       res.json(pick(user, ['_id', 'name', 'email']));
     })
